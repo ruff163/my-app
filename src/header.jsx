92e47b57213a70e227097e40b2b7e4d087eba859
ruff163 <screpi-du@mail.ru>
2022-03-06 21:09:23 +0400

    first commit

---
+++ src/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './header.css'
+
+
+const Header=()=>{
+    return(
+        <header className='header'>
+        <a href="#"><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/VK_Full_Logo_%282021-present%29.svg/1200px-VK_Full_Logo_%282021-present%29.svg.png'/></a>
+      </header>
+    )
+}
+export default Header;
